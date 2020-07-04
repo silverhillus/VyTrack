3cb1c35d2a27506ee3b4b2d6b07a6f1d8edddcfe
Ahm3tSY <yavuz.ahmetserdar@gmail.com>
2020-07-04 00:43:06 -0400

    v1

--- src/test/java/day01/test.java
+++ src/test/java/day01/test.java
@@ -2,8 +2,10 @@ package day01;
 
 
 public class test {
+    String name = "ahmet"
     public static void main(String[] args) {
         System.out.println("this is my first code");
         System.out.println("this is a test in branch coder");
+        System.out.println(name);
     }
 }
