4f0b7adf98a8905befa46b687410398c67b97559
Aysin Karaman <aysinkaraman@MacBook-Pro.local>
2020-07-04 00:43:07 -0400

    v1

--- src/test/java/day01/test.java
+++ src/test/java/day01/test.java
@@ -3,7 +3,9 @@ package day01;
 
 public class test {
     public static void main(String[] args) {
+        String name = "Aysin";
         System.out.println("this is my first code");
         System.out.println("this is a test in branch coder");
+        System.out.println(name);
     }
 }
