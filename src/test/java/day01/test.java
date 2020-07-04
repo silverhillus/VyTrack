39dbeb136be74421832fe2e0b557d245f1199a38
Koksal Ogec <ogeckoksal@gmail.com>
2020-07-04 00:54:04 -0400

    07_03_v2

--- src/test/java/day01/test.java
+++ src/test/java/day01/test.java
@@ -5,7 +5,7 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("hello hello world");
-
+        System.out.println("hello hello world");
         System.out.println("this is my first code");
         System.out.println("this is a test in branch coder");
     }
